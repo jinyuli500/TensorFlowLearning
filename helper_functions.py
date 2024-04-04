a4ec8964a61426af6d07b3fd38c124e6db8c98d3
jinyuli500 <159606121+jinyuli500@users.noreply.github.com>
2024-04-04 17:04:45 -0400

    change indent

--- helper_functions.py
+++ helper_functions.py
@@ -4,38 +4,39 @@ import zipfile
 import tensorflow as tf
 
 def plot_loss_curves(history):
-  """
-  return separated loss curves for training and validation metrics
-  """
-  loss = history.history["loss"]
-  val_loss = history.history["val_loss"]
-  accuracy = history.history["accuracy"]
-  val_accuracy = history.history["val_accuracy"]
+    """
+    return separated loss curves for training and validation metrics
+    """
 
-  epochs = range(len(history.history["loss"]))
+    loss = history.history["loss"]
+    val_loss = history.history["val_loss"]
+    accuracy = history.history["accuracy"]
+    val_accuracy = history.history["val_accuracy"]
 
-  # plot
-  plt.plot(epochs, loss, label="training_loss")
-  plt.plot(epochs, val_loss, label="val_loss")
-  plt.title("loss")
-  plt.xlabel("Epochs")
-  plt.legend()
+    epochs = range(len(history.history["loss"]))
 
-  plt.plot(epochs, accuracy, label="accuracy")
-  plt.plot(epochs, val_accuracy, label="val_accuracy")
-  plt.title("accuracy")
-  plt.xlabel("Epochs")
-  plt.legend()
+    # plot
+    plt.plot(epochs, loss, label="training_loss")
+    plt.plot(epochs, val_loss, label="val_loss")
+    plt.title("loss")
+    plt.xlabel("Epochs")
+    plt.legend()
+
+    plt.plot(epochs, accuracy, label="accuracy")
+    plt.plot(epochs, val_accuracy, label="val_accuracy")
+    plt.title("accuracy")
+    plt.xlabel("Epochs")
+    plt.legend()
 
 
 def create_tensorboard_callback(dir_name, experiment_name):
-  log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%D-%H%M%S")
-  tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
-  print(f"saving tensorboard log files to: {log_dir}")
-  return tensorboard_callback
+    log_dir = dir_name + "/" + experiment_name + "/" + datetime.datetime.now().strftime("%Y%m%D-%H%M%S")
+    tensorboard_callback = tf.keras.callbacks.TensorBoard(log_dir=log_dir)
+    print(f"saving tensorboard log files to: {log_dir}")
+    return tensorboard_callback
 
 
 def unzip_data(filename):
-  zip_ref = zipfile.ZipFile(filename)
-  zip_ref.extractall()
-  zip_ref.close()
+    zip_ref = zipfile.ZipFile(filename)
+    zip_ref.extractall()
+    zip_ref.close()
